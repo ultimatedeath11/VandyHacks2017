03ef0e74a058e3be35c2e97ecf77955f1307d203
JoelWhite16 <white15joel@gmail.com>
2017-10-20 23:46:34 -0500

    Switch to javascript

--- environment.py
+++ environment.py
@@ -6,14 +6,23 @@
 ##
 class Environment():
     def __init__(self):
-        print("Environment Init Function: {")
         ##initialize environmental variables:
         self.objects = []
-        self.objects.append({})
-        print("}")
+        self.ground = Plane(0,0,0,100,100)
+        self.objects.append(self.ground)
     def update(self):
         ##print("Update Environment...")
         self.animate()
     def animate(self):
         for obj in self.objects:
             print("Animate: "+str(obj))
+            print(obj.draw())
+class Plane():
+    def __init__(self,x,y,z,length,width):
+        self.x = x
+        self.y = y
+        self.z = z
+        self.length = length
+        self.width = width
+    def draw(self):
+        return [self.x,self.y,self.z],self.length,self.width
---
+++ main.js
@@ -0,0 +1,12 @@
+/*
+Create classes: Environment, Event and Event's Children, each blender object in the lib file
+
+*/
+function Environment(){
+    this.x = 0;
+    this.y = 0;
+    this.z = 0;
+    this.length = 100;
+    this.width = 100;
+    alert("environment created!");
+}
