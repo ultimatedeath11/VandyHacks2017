6cf91ded3783e3dbdf93c08d6e2db79e383d420e
JoelWhite16 <white15joel@gmail.com>
2017-10-21 01:25:22 -0500

    updates to main.html and main.js

--- main.js
+++ main.js
@@ -3,10 +3,6 @@ Create classes: Environment, Event and Event's Children, each blender object in 
 
 */
 function Environment(){
-    this.x = 0;
-    this.y = 0;
-    this.z = 0;
-    this.length = 100;
-    this.width = 100;
-    alert("environment created!");
+    this.ground;
+    this.objects = [];
 }
