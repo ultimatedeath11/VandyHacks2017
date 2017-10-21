37460c4278842c5e7be21437e1cce92398c913fd
JoelWhite16 <white15joel@gmail.com>
2017-10-20 22:15:00 -0500

    mainloop run/time

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 #very basic main python program that will be the core of the program, will run all other python files and return the output to the website. 
 import time
-class Main(runtime):
-    running = True
-    time = runtime
-    current_time = 0
-    def mainloop():
-        while running:
-            if current_time == runtime:
-                running = False
-            current_time +=1
+class Main():
+    def __init__(self,runtime):
+        self.running = True
+        self.time = runtime
+        self.current_time = 0
+    def mainloop(self):
+        while self.running:
+            if self.current_time == self.time:
+                self.running = False
+            print("Time: "+str(self.current_time))
+            self.current_time +=1
+            time.sleep(1)
 class Environment():
     def __init__(self):
         print("Environment Init Function: {")
