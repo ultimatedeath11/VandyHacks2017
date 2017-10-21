c30551ce9848dac9b65130d335366fcf01b02c8f
JoelWhite16 <white15joel@gmail.com>
2017-10-20 22:36:48 -0500

    Environment Class moved to its own file and imported successfully by main

---
+++ environment.py
@@ -0,0 +1,13 @@
+## Environment Class:
+##     Requires:
+##          Method update()
+##          Method animate()
+##          array of objects within environment
+##
+class Environment():
+    def __init__(self):
+        print("Environment Init Function: {")
+        ##initialize environmental variables:
+        print("}")
+    def update(self):
+        print("Update Environment...")
--- main.py
+++ main.py
@@ -1,22 +1,19 @@
 #very basic main python program that will be the core of the program, will run all other python files and return the output to the website. 
 import time
+import environment
 class Main():
     def __init__(self,runtime):
         self.running = True
         self.time = runtime
         self.current_time = 0
+        self.environment = environment.Environment()
     def mainloop(self):
         while self.running:
             if self.current_time == self.time:
                 self.running = False
+            self.environment.update()
             print("Time: "+str(self.current_time))
             self.current_time +=1
             time.sleep(1)
-class Environment():
-    def __init__(self):
-        print("Environment Init Function: {")
-        ##initialize environmental variables:
-        print("}")
-new_environment = Environment()
-main = Main(10) #ending number
+main = Main(int(input("Enter # of days: "))) #ending number
 main.mainloop()
