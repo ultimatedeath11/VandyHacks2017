392aa1aed54423a90d83f1953b6eb61ea433a0f1
JoelWhite16 <white15joel@gmail.com>
2017-10-20 22:11:14 -0500

    Starting Declarations

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
 #very basic main python program that will be the core of the program, will run all other python files and return the output to the website. 
+import time
+class Main(runtime):
+    running = True
+    time = runtime
+    current_time = 0
+    def mainloop():
+        while running:
+            if current_time == runtime:
+                running = False
+            current_time +=1
+class Environment():
+    def __init__(self):
+        print("Environment Init Function: {")
+        ##initialize environmental variables:
+        print("}")
+new_environment = Environment()
+main = Main(10)
+main.mainloop()
