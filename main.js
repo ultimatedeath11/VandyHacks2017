1f553f2f1556d50f625a733a30bdf1c346b790c3
JoelWhite16 <white15joel@gmail.com>
2017-10-22 01:39:33 -0500

    1:30

--- main.js
+++ main.js
@@ -9,11 +9,24 @@ function Environment(){
     this.objects = []; // array to hold objects for reference
     this.animateObjects = []; // array for all moving objects
     this.terrainQuadrants = [];
+    //update function runs world
+    this.update = function(){
+        this.animateObjects.forEach(function(obj) {
+            obj.animate();
+        }, this);
+    }
 }
 //Generic Event Function:
 function Event(){
     this.environmentalVariables = [];
     this.environmentalChanges = [];
+}
+//Genaric Animate Methods:
+function Prey(obj){
+    
+}
+function Predators(obj){
+    
 }
 //Generic Object Classes:
 function Mountain(x,y,z){
@@ -66,11 +79,44 @@ function Tree(x,y,z){
     this.scale = 10;
     this.path = '..//Blender .js files/finished_tree.js';
 }
-function Animal(x,y,z){
+
+function Cow(x,y,z){
     this.object = new THREE.Mesh();
     this.x = x;
     this.y = y;
     this.z = z;
     this.scale = 10;
-    this.path = '..//Blender .js files/cow-mini.js'
+    this.path = '/Blender .js files/cow.js';
+}
+function Horse(x,y,z){
+    this.object = new THREE.Mesh();
+    this.x = x;
+    this.y = y;
+    this.z = z;
+    this.scale = 10;
+    this.path = '/Blender .js files/horse.js';
+}
+function Goat(x,y,z){
+    this.object = new THREE.Mesh();
+    this.x = x;
+    this.y = y;
+    this.z = z;
+    this.scale = 10;
+    this.path = '/Blender .js files/goat.js';
+}
+function Bear(x,y,z){
+    this.object = new THREE.Mesh();
+    this.x = x;
+    this.y = y;
+    this.z = z;
+    this.scale = 10;
+    this.path = '/Blender .js files/bear.js';
+}
+function Wolf(x,y,z){
+    this.object = new THREE.Mesh();
+    this.x = x;
+    this.y = y;
+    this.z = z;
+    this.scale = 10;
+    this.path = '/Blender .js files/wolf.js';
 }
