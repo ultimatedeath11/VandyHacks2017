06fbdc9b5b25a4bafff65655898f832e461b4b9f
Noah Franck <ultimatedeath11@icloud.com>
2017-10-22 06:24:22 -0500

    created from copy and paste preditor into prey

--- main.js
+++ main.js
@@ -2,8 +2,10 @@
 Create classes: Environment, Event and Event's Children, each blender object in the lib file
 
 */
+console.log("main.js loaded correctly");
 var terrainScale = 3;
 function Environment(){
+    console.log("main.js loaded correctly");
     //introduce environmental variables here:
     this.ground; //base plane for world
     this.objects = []; // array to hold objects for reference
@@ -21,14 +23,7 @@ function Environment(){
     }
 }
 // ---Genaric Animate Methods---
-function Prey(obj){
-    /*
-        Determine movement based on:
-        1) Food
-        2) Safety/avoid predators
-        3) Proximity of others of the same species
-    */
-}
+
 function Predators(obj){
     /*
         Determine movement based on:
@@ -76,8 +71,61 @@ function Predators(obj){
         choice = options[choice];
         var coords = quadCoordinates(environment.terrainQuadrants[choice]);
         move(obj,coords);
+
     });
     console.log(options);
+}
+
+function Prey(obj){
+    /*
+    Determine movement based on:
+    1) Prey/Food
+    2) Shelter
+    3) Proximity of others of the same species
+*/
+//determine movement options:
+var options = [];
+var add;
+var keep;
+environment.terrainQuadrants.forEach(function(q){
+    add = false;
+    var iOfQ = environment.terrainQuadrants.indexOf(q); // set var to index of Quadrant
+    var iOfObj = environment.terrainQuadrants.indexOf(obj.quadrant);//set var to index of obj.quadrant in terrain quadrants
+    if(iOfQ-1 == iOfObj||iOfQ+1 == iOfObj ||iOfQ-4 == iOfObj ||iOfQ+4 == iOfObj){
+        add = true;
+    }
+});
+//narrow down option list based on food, shelter, others
+options.forEach(function(option){
+
+    keep = false;
+    //3) shelter TODO go over this
+    if(environment.terrainQuadrants[options].__class__ == obj.home){
+        keep = true;
+    }else{
+        options.remove(environment.terrainQuadrants.indexOf(environment.terrainQuadrants[options]));
+    }
+    environment.objectsInQuadrant[option].forEach(function(o){
+        //3) Food
+        if(o.class=='prey'){
+            keep = true;
+        }
+        //2) others TODO classes may be the death of us
+        if(o.__class__==obj.__class__&&keep!=true){
+            keep = true;
+        }
+        if(!keep){
+            options.remove(environment.terrainQuadrants.indexOf(environment.terrainQuadrants[options]));
+        }
+    });
+    //make random choice based on remaining options
+    var choice = Math.random()*options;
+    choice = options[choice];
+    var coords = quadCoordinates(environment.terrainQuadrants[choice]);
+    move(obj,coords);
+
+});
+console.log(options);
 }
 function quadCoordinates(quadrant){
     var x = (quadrant.x - 30) + (60*Math.random());
